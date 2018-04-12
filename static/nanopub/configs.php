55f0dafef6814e022a2748722503472849df5585
Kaushal Modi <kaushal.modi@gmail.com>
2018-04-12 15:26:18 -0400

    Update the nanopub config will minimal information

--- static/nanopub/configs.php
+++ static/nanopub/configs.php
@@ -1,37 +1,44 @@
 <?php
 
 return (object) array(
-	// First some settings for the site
-	'siteUrl' => 'https://example.com/',			// the URL for your site - note trailing slash
-	'timezone' => 'Europe/London',					// http://php.net/manual/en/timezones.php
-	'mediaPoint' => 'https://media.org/endpoint',	// Micropub Media Endpoint
-	
-	// Config Block for Twitter
-	'twitterName' => 'poopyCakes',					// your twitter account name, don't use the @
-	'twAPIkey' => 'WomtvR2YoT',						// Create an app on dev.twitter.com for your account.
-	'twAPIsecret' => 'NILIDJXg1e',					// APIkey & APIsecret are the APP's key & Secret
-	'twUserKey' => 'ILs4jUS7a6',					// UserKey & User Secret are under 'Your access token'
-	'twUserSecret' => 'NYbGUfuNUh',					// Generate those on dev.twitter.com
+    // First some settings for the site.
+    'siteUrl' => 'https://scripter.co/',                // the URL for your site - note trailing slash
+    'timezone' => 'America/New_York',                   // http://php.net/manual/en/timezones.php
+    // -- Thu Apr 12 15:19:52 EDT 2018 - kmodi
+    // What is the mediaPoint?
+    'mediaPoint' => 'https://media.org/endpoint',       // Micropub Media Endpoint
 
-	// Config Block for Mastodon
-	'mastodonInstance' => 'servername.ext',			// your Mastodon Instance
-	'mastodonToken' => 'uWo42Bca91',				// get an auth code using Mastodon docs
+    // // Config Block for Twitter
+    // -- Thu Apr 12 15:21:58 EDT 2018 - kmodi
+    // Not sure if the twitter api stuff can be committed in public. Most likely not.
+    // 'twitterName' => 'kaushalmodi',                  // your twitter account name, don't use the @
+    // 'twAPIkey' => 'WomtvR2YoT',                      // Create an app on dev.twitter.com for your account.
+    // 'twAPIsecret' => 'NILIDJXg1e',                   // APIkey & APIsecret are the APP's key & Secret
+    // 'twUserKey' => 'ILs4jUS7a6',                     // UserKey & User Secret are under 'Your access token'
+    // 'twUserSecret' => 'NYbGUfuNUh',                  // Generate those on dev.twitter.com
 
-	// Config for micro.blog
-	'pingMicro' => True, 							// Set to False (boolean) if you don't use micro.blog
-	'siteFeed' => 'https://example.com/atom.xml',	// Set to your site's RSS/Atom Feed to notify micro.blog
+    // Config Block for Mastodon
+    // -- Thu Apr 12 15:23:36 EDT 2018 - kmodi
+    // Not sure if the mastodon api stuff can be committed in public. Most likely not.
+    // Also, where do I get my mastodon token from?
+    // 'mastodonInstance' => 'servername.ext',          // your Mastodon Instance
+    // 'mastodonToken' => 'uWo42Bca91',                 // get an auth code using Mastodon docs
 
-	// Config for Weather. If you do want weather feature, set to true 
+    // Config for micro.blog
+    'pingMicro' => True,                                // Set to False (boolean) if you don't use micro.blog
+    'siteFeed' => 'https://scripter.co/atom.xml',       // Set to your site's RSS/Atom Feed to notify micro.blog
+
+    // Config for Weather. If you do want weather feature, set to true.
     'weatherToggle' => false,
-	'compass' => 'https://private.tracker.com/api',
-	'compassKey' => 'PrivateAPIkey',
-	'forecastKey' => 'DarkSkyApiKey',
-	'defaultLat' => '51.5074',
-	'defaultLong' => '0.1278',
-	'defaultLoc' => 'London',
+    'compass' => 'https://private.tracker.com/api',
+    'compassKey' => 'PrivateAPIkey',
+    'forecastKey' => 'DarkSkyApiKey',
+    'defaultLat' => '51.5074',
+    'defaultLong' => '0.1278',
+    'defaultLoc' => 'London',
 
-	// Set Frontmatter Format -- json or yaml
-	'frontFormat' => 'json'
+    // Set Frontmatter Format -- json or yaml
+    'frontFormat' => 'json'
 );
 
 ?>
